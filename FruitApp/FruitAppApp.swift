59c93e68c5380ce26b314ce44a1a17d34a85515b
ehsan2793 <ehsan2793@gmail.com>
2023-06-03 16:07:28 -0700

    Initial Commit

---
+++ FruitApp/FruitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitAppApp.swift
+//  FruitApp
+//
+//  Created by Ehsan Rahimi on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
