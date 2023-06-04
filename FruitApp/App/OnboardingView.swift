1b027b07d2e9401123a52ed3d76fbaa624f2cf73
ehsan2793 <ehsan2793@gmail.com>
2023-06-03 22:31:00 -0700

    added tabview

---
+++ FruitApp/App/OnboardingView.swift
@@ -0,0 +1,32 @@
+//
+//  OnboardingView.swift
+//  FruitApp
+//
+//  Created by Ehsan Rahimi on 6/3/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+
+    // MARK: - BODY
+
+    var body: some View {
+        TabView {
+            ForEach(0 ..< 5) { _ in
+                FruitCardView()
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
